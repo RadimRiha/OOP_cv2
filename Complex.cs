83a5a2ac714cb5b35e0dc1f278b384eff4199c71
RadimRiha <radim.rihacek@seznam.cz>
2021-02-18 15:42:27 +0100

    Tweaks
    Argument method fixed
    Separated classes into files

---
+++ Complex.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_cv2
+{
+    public class Complex
+    {
+        public double Real;
+        public double Imag;
+        public const double Epsilon = 1E-6; //maximum error for == and != operators
+        static private char symbol = 'i';    //imaginary symbol
+
+        //constructor
+        public Complex(double real, double imag)
+        {
+            Real = real;
+            Imag = imag;
+        }
+
+        //operators
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a.Real + b.Real, a.Imag + b.Imag);
+        }
+        public static Complex operator -(Complex a, Complex b)
+        {
+            return new Complex(a.Real - b.Real, a.Imag - b.Imag);
+        }
+        public static Complex operator *(Complex a, Complex b)
+        {
+            return new Complex(a.Real * b.Real - a.Imag * b.Imag, a.Imag * b.Real + a.Real * b.Imag);
+        }
+        public static Complex operator /(Complex a, Complex b)
+        {
+            return new Complex((a.Real * b.Real + a.Imag * b.Imag) / (b.Real * b.Real + b.Imag * b.Imag),
+                               (a.Imag * b.Real - a.Real * b.Imag) / (b.Real * b.Real + b.Imag * b.Imag));
+        }
+        public static bool operator ==(Complex a, Complex b)
+        {
+            return (Math.Abs(a.Real - b.Real) < Epsilon && Math.Abs(a.Imag - b.Imag) < Epsilon);
+        }
+        public static bool operator !=(Complex a, Complex b)
+        {
+            return !(a == b);
+        }
+        public static Complex operator -(Complex a)
+        {
+            return new Complex(-a.Real, -a.Imag);
+        }
+        public override string ToString()
+        {
+            return string.Format("{0:F2}{1}{2:F2}{3}", Real, Imag >= 0 ? "+" : "", Imag, symbol);
+        }
+        public Complex Conjugate()
+        {
+            return new Complex(Real, -Imag);
+        }
+        public double Modulus()
+        {
+            return Math.Sqrt(Real * Real + Imag * Imag);
+        }
+        public double Argument()
+        {
+            return Math.Atan2(Imag, Real);
+        }
+    }
+}
---
+++ TestComplex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_cv2
+{
+    public class TestComplex
+    {
+        public static void Test(Complex actual, Complex expected, string testName)
+        {
+            if (actual == expected) Console.WriteLine("{0}: OK", testName);
+            else Console.WriteLine("{0}: Error, Expected value: {1} Actual value: {2}", testName, expected, actual);
+        }
+    }
+}
